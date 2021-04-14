ab8150d36add9e10c3fd5babc1165c26062213fc
dudikpal <dudikpal@gmail.com>
2021-04-14 18:21:28 +0200

    Java - Easy - Find the Discount task

---
+++ src/main/java/veryeasy/findthediscount/Sales.java
@@ -0,0 +1,9 @@
+package veryeasy.findthediscount;
+
+public class Sales {
+    
+    public static double discount(int price, int percentage) {
+    
+        return price - (price * percentage * 0.01);
+    }
+}
