93d09b452f3a410f7e4273f17444f226eb0938f5
dudikpal <dudikpal@gmail.com>
2021-04-14 17:02:13 +0200

    Java - Very Easy - Return the Next Number from the Integer Passed task

---
+++ src/main/java/veryeasy/returnthenextnumberfromtheintpassed/Challenge.java
@@ -0,0 +1,9 @@
+package veryeasy.returnthenextnumberfromtheintpassed;
+
+public class Challenge {
+    
+    public static int addition(int num) {
+    
+        return ++num;
+    }
+}
---
+++ src/test/java/veryeasy/returnthenextnumberfromtheintpassed/ChallengeTest.java
@@ -0,0 +1,39 @@
+package veryeasy.returnthenextnumberfromtheintpassed;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChallengeTest {
+    
+    @Test
+    public void test1() {
+        System.out.println("2 plus 1 equals 3.");
+        assertEquals(3, Challenge.addition(2));
+    }
+    
+    @Test
+    public void test2() {
+        System.out.println("-8 plus 1 equals 9.");
+        assertEquals(-8, Challenge.addition(-9));
+    }
+    
+    @Test
+    public void test3() {
+        System.out.println("0 plus 1 equals 1.");
+        assertEquals(1, Challenge.addition(0));
+    }
+    
+    @Test
+    public void test4() {
+        System.out.println("999 plus 1 equals 1000.");
+        assertEquals(1000, Challenge.addition(999));
+    }
+    
+    @Test
+    public void test5() {
+        System.out.println("73 plus 1 equals 74.");
+        assertEquals(74, Challenge.addition(73));
+    }
+
+}
