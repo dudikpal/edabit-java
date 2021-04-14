2b6f351e1239a06f485fd7f3be2f3d1965b4dafd
dudikpal <dudikpal@gmail.com>
2021-04-14 17:31:43 +0200

    Java - Easy - Triangular Number Sequence task

---
+++ src/test/java/veryeasy/triangularnumbersequence/ChallengeTest.java
@@ -0,0 +1,34 @@
+package veryeasy.triangularnumbersequence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChallengeTest {
+    
+    @Test
+    public void test1() {
+        assertEquals(1, Challenge.triangle(1));
+    }
+    
+    @Test
+    public void test2() {
+        assertEquals(3, Challenge.triangle(2));
+    }
+    
+    @Test
+    public void test3() {
+        assertEquals(6, Challenge.triangle(3));
+    }
+    
+    @Test
+    public void test4() {
+        assertEquals(36, Challenge.triangle(8));
+    }
+    
+    @Test
+    public void test5() {
+        assertEquals(2318781, Challenge.triangle(2153));
+    }
+
+}
